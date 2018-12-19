fb608620fc97430891594b3055c7e72ffc286225
Luke <lwolff@users.noreply.github.com>
2018-12-19 15:44:03 -0500

    Day 3
    kind of hacky, but wanted to be quick for scoreboard attempt

---
+++ Day3_part1.py
@@ -0,0 +1,31 @@
+import re
+
+f = open("Day3_input.txt", 'r')
+
+x = []
+
+for i in f:
+    x.append(re.findall('\d+', i))
+
+w = 0
+h = 0
+for i in x:
+    if (int(i[1]) + int(i[3])) > w:
+        w = (int(i[1]) + int(i[3]))
+    if (int(i[2]) + int(i[4])) > h:
+        h = (int(i[2]) + int(i[4]))
+
+fabric = [[0 for x in range(w + 1)] for y in range(h + 1)]
+
+for i in x:
+    for j in range(int(i[3])):
+        for k in range(int(i[4])):
+            fabric[int(i[1]) + j - 1][int(i[2]) + k - 1] += 1
+
+ans = 0
+for j in range(w):
+    for k in range(h):
+        if fabric[j][k] > 1:
+            ans += 1
+
+print(ans)
